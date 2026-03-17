be4dafffe1ebcb59362396a341701b98050dcda1
mrdollarhere <mrdollarhere@gmail.com>
2026-03-17 19:56:08 +0700

    add the API URL GS

--- js/config.js
+++ js/config.js
@@ -3,6 +3,6 @@
 //  ⚠️  Chỉ cần sửa file này khi đổi Apps Script URL
 // ============================================================
 
-const SCRIPT_URL = '';
+const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbx0YSPyzR7wvG_nxMS15ALWP6Z6yTAl6m4OdlYeQA79N9Oqfcyv7kTEYwg_qSKXgN-o/exec';
 
 const ADMIN_PASSWORD = 'admin123'; // ← đổi mật khẩu admin tại đây
